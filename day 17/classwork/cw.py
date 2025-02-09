dc62e466d0e10aaca0e495b43bcdfb1edb6f45d0
barbaredenozashvili <barbaredvnoza21@gmail.com>
2025-02-09 17:52:19 +0400

    commit

---
+++ day 17/classwork/cw.py
@@ -0,0 +1,29 @@
+# 1) for loop-ში 
+# გამოიტანეთ 950-დან 1000-ის ჩათვლით ლუწი რიცხვები. 
+# 2) for loop-ში გამოიტანეთ თქვენი სახელი 100-ჯერ
+# 3) for loop-ში გამოიტანე "Goa Is the best" 1000-ჯერ                            
+# 4) for loop-ში გამოიტანე რიცხვები 100-დან 200-მდე         
+
+
+
+for x in range(950, 1000, 2):
+    print(x)
+for i in range(100):
+    print("barbare")
+for i in range(1000):
+    print("Goa Is the best")
+for x in range(100, 200,):
+    print(x)
+
+
+# 5)დაწერეთ 200-250 მდე მარტო 15 ის ჯერადი რიცხვები
+# 6)დაწერეთ 200 იდან 400 მდე რიცხვები
+
+
+
+for x in range(200, 250, 15):
+    print(x)
+for x in range(200, 400):
+    print(x)
+
+    
