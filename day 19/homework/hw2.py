f56f78b62ba3e16e5e917182982058fb28ec84d4
barbaredenozashvili <barbaredvnoza21@gmail.com>
2025-03-03 14:30:15 +0400

    commit

---
+++ day 19/homework/hw2.py
@@ -0,0 +1,12 @@
+# 2️ დაბეჭდეთ "GOA BEST!!!" 100-ჯერ, ორივე ციკლით შეასრულეთ ეს დავა
+
+# 1
+i = 0
+
+while i < 100:
+    print("GOA BEST!!!")
+
+# 2
+
+for i in range(100):
+    print("GOA BEST!!!")
